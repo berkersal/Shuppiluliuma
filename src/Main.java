a9596256de3aa4f656cdb33407613ee1608d09e5
berkersal <brkr.sal@gmail.com>
2020-02-20 11:53:11 +0300

    test7

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("TEST3");
-        System.out.println("ali7");
+        System.out.println("TEST4");
     }
 }
