769ee3b22baa21cf7a695bc937b0423299811792
Zafer <Zafer@K555L>
2020-02-20 18:05:26 +0300

    test8

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("TEST3");
         System.out.println("TEST4");
-        System.out.println("TEST6");
+        System.out.println("TEST8");
     }
 }
