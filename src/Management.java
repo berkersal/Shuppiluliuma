a7e6fe3fc16133d73161aac0a467dc19048e29d6
berkersal <brkr.sal@gmail.com>
2020-02-20 23:53:15 +0300

    main for fin

--- src/Management.java
+++ src/Management.java
@@ -23,7 +23,28 @@ public class Management {
 	public void MAINFOR(Library[] libraries){
 		int index = 0;
 		for (int i = 0; i < getDeadLineTime(); i++) {
-			libraries[index] = null;
+			if (libraries[index].signupTime > 0){
+				signUp(libraries[index]);
+				for (int j = 0; j < libraries[index].signupTime; j++)
+					scan(libraries);
+			}
+			index++;
+		}
+	}
+	public void signUp(Library lib){
+		lib.signupTime = 0;
+	}
+	public void scan(Library[] libraries){
+		int index = 0;
+		while (libraries[index].signupTime != 0){
+			scanBestBook(libraries[index]);
+
+			index++;
+		}
+	}
+	public void  scanBestBook(Library lib){
+		for (int i = 0; i < lib.bookPerDay; i++) {
+			lib.booksent.add(lib.books[i]);
 		}
 	}
 	
