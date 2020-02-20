64b8598b43dde0a1958c6f98b71bd16ac8d1fbfd
Zafer <Zafer@K555L>
2020-02-20 22:04:16 +0300

    main cleared

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        FileReading fileReading = new FileReading();
-
+    public static void main(String[] args){
+    
     }
 }
