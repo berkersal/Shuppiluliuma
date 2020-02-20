89c4ee2c19cd334dd0f56263532a78f232004db1
berkersal <brkr.sal@gmail.com>
2020-02-20 19:35:45 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("adasdasd");
-        System.out.println("mert");
+        System.out.println("Code Starts Here!");
     }
 }
