d4cd9efdc984df3075d0e6508440fd96a03421d7
mertduver <mmert1505@gmail.com>
2020-02-20 18:41:19 +0300

    sa yaptım

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("adasdasd");
         System.out.println("deneme deneme");
+        System.out.println("deneme deneme");
     }
 }
