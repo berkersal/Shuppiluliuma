7c487c3c1c1aac6be437e41afd2d51a705ddc099
berkersal <brkr.sal@gmail.com>
2020-02-20 18:29:20 +0300

    test9

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("TEST3");
-        System.out.println("TEST4");
-        System.out.println("TEST8");
+        System.out.println("TEST9");
     }
 }
