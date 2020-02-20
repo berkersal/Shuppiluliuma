a835d1c37fc98bd33da8d4f617749d95fe7f8b36
mertduver <mmert1505@gmail.com>
2020-02-20 21:57:29 +0300

    mert

--- src/FileReading.java
+++ src/FileReading.java
@@ -1,2 +1,14 @@
+import java.io.*;
+
 public class FileReading {
+    public FileReading() throws IOException {
+        File file = new File("Files");
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+        while ((st = br.readLine()) != null)
+            System.out.println(st);
+    }
 }
+
